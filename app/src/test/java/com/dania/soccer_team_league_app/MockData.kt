9fbcc0b9a1fb50fee9ae5626755fefd23cdd7095
Dania <daniajibellis@hotmail.com>
2021-11-26 04:23:43 -0500

    Se agregan test unitarios para las extendsFuntions y para el viewmodel

---
+++ app/src/test/java/com/dania/soccer_team_league_app/MockData.kt
@@ -0,0 +1,21 @@
+package com.dania.soccer_team_league_app
+
+import com.dania.soccer_team_league_app.domain.model.Team
+import com.dania.soccer_team_league_app.domain.model.Teams
+
+val teams = Teams(
+    listOf(
+        Team(
+            name = "Alaves",
+            stadium = "metropolitano",
+            badge = "url de badged",
+            description = "Equipo de futbol",
+            formedYear = "1800",
+            jersey = "url de jersey",
+            website = null,
+            facebook = null,
+            instagram = null,
+            twitter = null
+        )
+    )
+)
